3f8418fd856fffd7ec4666f296bf975bad504234
Jbakesy99 <joshua-bakes@hotmail.co.uk>
2025-07-27 22:22:09 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+  const app = document.getElementById("app");
+  app.innerHTML = "<p>All core systems are fully functional.</p>";
+});
---
+++ service-worker.js
@@ -0,0 +1,16 @@
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("deadgod-cache").then(cache => {
+      return cache.addAll(["index.html", "style.css", "main.js", "manifest.json"]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
